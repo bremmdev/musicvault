2a557a69a1ea77a0a0ecd7341d758f272ee90919
Matt Bremmers <bremmdev@gmail.com>
2023-11-23 13:43:27 +0100

    add redirect for /bands + add active link styles

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,23 +1,30 @@
+"use client";
+
 import React from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Separator from "../ui/Separator";
 
 const links = [
-  { href: "/", label: "bands" },
+  { href: "/bands", label: "bands" },
   { href: "/albums", label: "albums" },
   { href: "/songs", label: "songs" },
   { href: "/collection", label: "collection" },
 ];
 
 const Navigation = () => {
+
+  const pathname = usePathname();
+
   return (
     <nav className="mx-auto bg-gradient-to-r from-slate-200 via-slate-50 to-slate-200 py-5">
       <ul className="flex justify-center gap-2 sm:gap-4 text-base sm:text-lg font-light">
         {links.map(({ href, label }, idx) => {
+          const isActive = href.startsWith(pathname);
           return (
             <React.Fragment key={label}>
               <li>
-                <Link href={href} className="nav-link relative">
+                <Link href={href} className={`nav-link ${isActive ? 'font-medium' : '' } relative`}>
                   {label}
                 </Link>
               </li>
