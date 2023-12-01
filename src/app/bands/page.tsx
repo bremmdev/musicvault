8c1ede14443de385c0117c4563dfef415476eff3
Matt Bremmers <bremmdev@gmail.com>
2023-12-01 22:52:04 +0100

    add suspense boundary for CreateOrEditBands

--- src/app/bands/page.tsx
+++ src/app/bands/page.tsx
@@ -3,6 +3,8 @@ export const dynamic = "force-dynamic";
 import { prisma } from "@/lib/db";
 import BandsTable from "@/components/bands/BandsTable";
 import CreateOrEditBand from "@/components/bands/CreateOrEditBand";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
 
 export default async function BandsPage() {
   const bands = await prisma.band.findMany({
@@ -15,11 +17,19 @@ export default async function BandsPage() {
     },
   });
 
+  const fallback = (
+    <div className="flex justify-center items-center my-8">
+      <Loader2 className="animate-spin h-10 w-10 text-slate-950" />
+    </div>
+  );
+
   return (
     <>
       <h1 className="text-3xl font-extralight text-center mt-12 mb-6">Bands</h1>
       <BandsTable bands={bands} />
-      <CreateOrEditBand />
+      <Suspense fallback={fallback}>
+        <CreateOrEditBand />
+      </Suspense>
     </>
   );
 }
