db6c58e3c02c3201da69eaf37158ac5cc213a2c2
Matt Bremmers <bremmdev@gmail.com>
2023-12-07 22:56:03 +0100

    refactor: remove session check on /collection

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,17 +1,9 @@
-export const revalidate = 0;
-
 import React from "react";
-import { getServerSession } from "next-auth/next";
-import { AuthOptions } from "@/app/api/auth/[...nextauth]/options";
 
 export default async function CollectionPage() {
-  const session = await getServerSession(AuthOptions);
-
-  return session ? (
-    <div>Collection</div>
-  ) : (
+  return (
     <div className="text-center my-16 font-medium">
-      Only admins are authorized to view this page
+      Collection
     </div>
   );
 }
